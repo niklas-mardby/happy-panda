ddc645eb7adf15f09ac0369937e81f6b9fce0501
Niklas Mårdby <niklas.mardby@gmail.com>
2024-09-04 14:54:50 +0200

    Stuff

--- public/app.js
+++ public/app.js
@@ -1,3 +1,32 @@
 console.log("Hello F24!");
 console.log("Hur mår ni?");
 console.log("Dags för rast!");
+
+// deklarera = skapa
+// tilldelar vi ett värde
+// initiera = tilldela samtidigt som vi deklarerar
+
+let x = 12;
+console.log(x);
+console.log(typeof x);
+x = "hello";
+console.log(x);
+console.log(typeof x);
+
+// const eller let
+
+let n = "Niklas";
+// console.log("Hello " + n);
+console.log(`Detta är en sträng ${n}`);
+
+// symbolen heter back tick
+
+// ÖVA!
+
+/**
+
+Deklarera en variabel med ditt namn, din ålder och en hobby. 
+Skriv en mening som använder template literal och console.log för att visa
+en läsbar mening med dina värden.
+
+ */
